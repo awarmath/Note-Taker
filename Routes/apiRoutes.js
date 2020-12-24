dd29e9d5cc094e020e234e22c4346af9bcc2a9a4
awarmath <andrew@carmunity.io>
2020-12-24 10:50:59 -0600

    attempt to correct apiRoutes

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,25 +1,32 @@
 const fs = require('fs');
-const { inherits } = require('util');
-let notes = [];
-var lastId = 0;
+const express = require('express');
+const notes = require('../db/db.json');
 
 init();
 
 module.exports = function(app) {
+    //setup the get route
     app.get("/api/notes", function(req, res) {
-    res.json(notes);
+        res.json(notes);
     });
 
+    //setup post route
     app.post("/api/index", function(req, res) {
         req.body.id = parseInt(lastId);
-        notes.push(req/body);
-        pushToJsonFile(notes);
+        notes.push(req.body);
+        fs.writeFileSync(notes);
         res.json(true);
     });
 
+    //Show a note by ID
+    app.get("/api/notes/:id", function(req, res) {
+        res.json(notes[req.params.id]);
+    });
+
+    //Delete a note by ID
     app.delete("/api/index/:id", function(req, res) {
-        var filteredNotes = notes.filter(note => note.id !== parseInt(req.params.id));
-        writeToJonFile(filteredNotes);
+        var filteredNotes = notes.filter(note => note.id !== parseInt(req.params.id)); 
+        fs.writeFileSync(filteredNotes);
         notes = filteredNotes;
         res.json(true);
     });
@@ -28,21 +35,22 @@ module.exports = function(app) {
 function init() {
     fs.readFile("./db/db.json", "utf8", function (err, data) {
         if (err) {
-            throw err;
+             throw err;
         }
         let notesJSON = JSON.parse(data);
         notesJSON.forEach(function (note) {
             notes.push(note);
         });
-        lastId = Math.max(...notes.map(obj => obj.id), 0) +1;
+        astId = Math.max(...notes.map(obj => obj.id), 0) + 1;
     });
 };
 
-function pushToJsonFile(notes) {
+function writeToJsonFile(notes) {
     let notesJSON = JSON.stringify(notes, null, 2);
-    fs.writeFile("./db/db.json", notesJSON, function (err) {
+    fs.writeFileSync("./db/db.json", notesJSON, function (err) {
         if (err) {
             throw err;
-        }
+         }
     });
 };
+
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+//require dependencies
 const path = require('path');
-const express = require('express');
+const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// add data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
+//imports routes for api and html
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+//add listener
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+    console.log(`API server now on port ${PORT}`);
 });
